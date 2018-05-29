d46b85bc0072684a425f3a1fb6f1cb23e322079d
kiminyeob <kiyeob4416@gmail.com>
2018-05-29 17:36:35 +0900

    타이머 서비스 구현 - Thread in Service로 구현함!

--- app/src/main/java/kr/ac/kaist/lockscreen/CountService.java
+++ app/src/main/java/kr/ac/kaist/lockscreen/CountService.java
@@ -7,18 +7,16 @@ import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class CountService extends Service {
-    protected CountDownTimer timer;
-    protected long duration = 20*1000;
-    protected long interval = 1000;
     protected SharedPreferences pref=null;
     protected SharedPreferences.Editor editor =null;
     private BroadcastReceiver mReceiver;
+    protected boolean isStop=  false;
+    protected int trigger_duration_in_second = 100;
 
     public CountService() {
     }
@@ -30,6 +28,11 @@ public class CountService extends Service {
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         mReceiver = new ScreenReceiver();
         registerReceiver(mReceiver, filter);
+
+        //카운터 시작
+        isStop = false;
+        Thread counter = new Thread(new Counter());
+        counter.start();
     }
 
     @Override
@@ -52,7 +55,7 @@ public class CountService extends Service {
                 .build();
         startForeground(9999, notiEx);
 
-        //Screen receiver로부터 Screen On/OFF event를 받을 수 있음
+        //Screen receiver 로부터 Screen On/OFF Event 를 받을 수 있음
         if( intent == null)
         {
             IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
@@ -61,43 +64,36 @@ public class CountService extends Service {
             registerReceiver(mReceiver, filter);
         }
 
-        //카운트 다운 시작
-        timer = new CountDownTimer(duration,interval) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                Log.i("Remaining time",Long.toString(millisUntilFinished));
-                editor.putInt("FocusMode",0);
-                editor.commit();
-                //Toast.makeText(getApplicationContext(), Long.toString(millisUntilFinished), Toast.LENGTH_LONG).show();
-            }
-            @Override
-            public void onFinish() {
-                //Toast.makeText(getApplicationContext(), "끝!", Toast.LENGTH_LONG).show();
-                editor.putInt("FocusMode",1);
-                editor.commit();
-
-                try {
-                    Thread.sleep(2*interval);
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-
-                Log.i("information","끝!");
-
-                /*
-                Intent intent = new Intent("kr.ac.kaist.lockscreen.TIMER_FINISHED");
-                intent.putExtra("DATA","COUNT_IS_FINISHED");
-                sendBroadcast(intent);
-                */
-            }
-        };
-        timer.start();
         return START_REDELIVER_INTENT;
     }
 
     @Override
     public void onDestroy(){
         super.onDestroy();
+        isStop = true;
         unregisterReceiver(mReceiver);
     }
+
+    private class Counter implements Runnable {
+        private int count = 0;
+        //private Handler handler = new Handler();
+
+        @Override
+        public void run() {
+            while(!isStop){
+                Log.i("test", String.valueOf(count++));
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if(count == trigger_duration_in_second){
+                    break;
+                }
+            }
+            Log.i("test", "Thread finished counter is at " + String.valueOf(count));
+        }
+    }
 }
+
+
